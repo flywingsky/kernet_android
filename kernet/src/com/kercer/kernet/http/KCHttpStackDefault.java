39063b6c715613d4740d013b5308c491d39f1643
zihong <zihong87@gmail.com>
2016-02-18 11:47:15 +0800

    connection setInstanceFollowRedirects

--- kernet/src/com/kercer/kernet/http/KCHttpStackDefault.java
+++ kernet/src/com/kercer/kernet/http/KCHttpStackDefault.java
@@ -135,6 +135,11 @@ public class KCHttpStackDefault implements KCHttpStack
 		URL parsedUrl = new URL(url);
 		HttpURLConnection connection = openConnection(parsedUrl, request);
 
+		// Workaround for the M release HttpURLConnection not observing the
+		// HttpURLConnection.setFollowRedirects() property.
+		// https://code.google.com/p/android/issues/detail?id=194495
+		connection.setInstanceFollowRedirects(HttpURLConnection.getFollowRedirects());
+
 		//process request cookies
 		mCookieManager.processRequest(request);
 
